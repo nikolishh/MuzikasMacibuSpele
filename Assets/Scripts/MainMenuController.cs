c89151278268d8b637b2dfa9327ead5073e62dde
Nikola Dundere <nikola.dundere1@gmail.com>
2026-04-26 21:59:39 +0300

    small fixes

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -4,42 +4,33 @@ using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
-    public Button quizLevel2Button;
-    public Button instrumentLevel2Button;
-
-    void Start()
-    {
-        if (quizLevel2Button != null)
-            quizLevel2Button.interactable = LevelSystem.IsLevelUnlocked("QuizGame", 2);
-
-        if (instrumentLevel2Button != null)
-            instrumentLevel2Button.interactable = LevelSystem.IsLevelUnlocked("InstrumentGame", 2);
-
-        Debug.Log("Quiz L1: " + PlayerPrefs.GetInt("QuizGame_Level1", 0));
-        Debug.Log("Quiz L2 unlocked: " + LevelSystem.IsLevelUnlocked("QuizGame", 2));
-    }
-
     public void LoadQuizGame()
     {
-        if (LevelSystem.IsLevelUnlocked("QuizGame", 1))
-            SceneManager.LoadScene("QuizGame");
+        SceneManager.LoadScene("QuizGame");
     }
 
     public void LoadQuizGame2()
     {
-        if (LevelSystem.IsLevelUnlocked("QuizGame", 2))
-            SceneManager.LoadScene("QuizGame2");
+        SceneManager.LoadScene("QuizGame2");
+    }
+
+    public void LoadQuizGame3()
+    {
+        SceneManager.LoadScene("QuizGame3");
     }
 
     public void LoadInstrumentGame()
     {
-        if (LevelSystem.IsLevelUnlocked("InstrumentGame", 1))
-            SceneManager.LoadScene("InstrumentGame");
+        SceneManager.LoadScene("InstrumentGame");
     }
 
     public void LoadInstrumentGame2()
     {
-        if (LevelSystem.IsLevelUnlocked("InstrumentGame", 2))
-            SceneManager.LoadScene("InstrumentGame2");
+        SceneManager.LoadScene("InstrumentGame2");
+    }
+
+    public void LoadInstrumentGame3()
+    {
+        SceneManager.LoadScene("InstrumentGame3");
     }
 }
